82ce25e21c0cf016870899df6ed56743cf9cee3b
Kalyan Saha <klynsaha@gmail.com>
2021-12-31 21:31:44 +0530

    added add/update/delete feedback to backend

--- src/Context/FeedbackContext.jsx
+++ src/Context/FeedbackContext.jsx
@@ -12,22 +12,32 @@ export const FeedbackProvider = ({ children }) => {
     });
 
     useEffect(async () => {
-        const response = await fetch(
-            "http://localhost:5001/Feedback?_sort=id&_order=desc"
-        );
+        const response = await fetch("/feedback?_sort=id&_order=desc");
         const data = await response.json();
+
         setFeedbacks(data);
         setIsLoading(false);
     }, []);
 
-    const addFeedback = (newFeedback) => {
-        console.log(newFeedback);
-        newFeedback.id = uuidv4();
-        setFeedbacks([newFeedback, ...feedbacks]);
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch("/feedback", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newFeedback),
+        });
+        const data = await response.json();
+
+        setFeedbacks([data, ...feedbacks]);
     };
 
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if (!window.confirm("Are you sure you want to delete ?")) return;
+        await fetch(`/feedback/${id}`, {
+            method: "DELETE",
+        });
+
         setFeedbacks((feedbacks) => {
             return feedbacks.filter((feedback) => feedback.id !== id);
         });
@@ -40,10 +50,19 @@ export const FeedbackProvider = ({ children }) => {
         });
     };
 
-    const updateFeedback = (id, updatedFeedback) => {
+    const updateFeedback = async (id, updatedFeedback) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedFeedback),
+        });
+        const data = await response.json();
+
         setFeedbacks(
             feedbacks.map((item) =>
-                item.id === id ? { ...item, ...updatedFeedback } : item
+                item.id === id ? { ...item, ...data } : item
             )
         );
     };
